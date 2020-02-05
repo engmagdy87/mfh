e3c77ecf025a6de428408c753341d9031ffb5795
engmagdy87 <mohamed.magdy.abdelhamid@gmail.com>
2020-02-05 18:20:13 +0200

    scroll behavior polyfill

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import router from './router';
 
+import smoothscroll from 'smoothscroll-polyfill';
+
+// kick off the polyfill!
+smoothscroll.polyfill();
+
 const App = () => import('./App.vue');
 
 Vue.use(VueRouter);
